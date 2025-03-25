98e69be6fe89c6ec02fba677fb43e23106fbf040
lnventorsteve <dylanv60431@gmail.com>
2025-03-24 21:01:40 -0500

    Added a rotation class

--- src/sm_blueprint_lib/rot.py
+++ src/sm_blueprint_lib/rot.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
 from typing import Sequence
+from constants import ROTATIONS
 
 
 @dataclass
 class Rot:
     """Class that represents the rotation of a block (x-axis, y-axis)
     """
-    x: int
-    y: int
+    x_axis: int
+    y_axis: int
 
     def __add__(self, o: "Rot" | Sequence):
         if isinstance(o, Rot):
@@ -15,7 +16,7 @@ class Rot:
         return Rot(self.x + o[0], self.y + o[1])
 
 
-def check_pos(rot: Sequence | dict) -> Rot:
+def check_rot(rot: Sequence | dict) -> Rot:
     """Converts a Sequence or dict into a Rot class if it wasn't already.
 
     Args:
@@ -30,3 +31,22 @@ def check_pos(rot: Sequence | dict) -> Rot:
         else:
             rot = Rot(**rot)
     return rot
+
+
+def set_rotation(pos,rot, facing, rotated):
+    """Sets the rotation of a block
+
+    Args:
+        pos (Pos): Class that represents the position of a block (x, y, z)
+        rot (Rot): Class that represents the rotation of a block (x-axis, y-axis)
+        facing (String): String indicating facing direction of the block
+        rotated (String): String indicating rotated direction of the face
+
+    """
+    x, y, z, x_axis, y_axis = ROTATIONS.rotations[facing][rotated]
+    pos.x += x
+    pos.x += y
+    pos.x += z
+    rot.x_axis = x_axis
+    rot.y_axis = y_axis
+
